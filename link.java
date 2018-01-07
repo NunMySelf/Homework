e2ebf485cf26a862a0b0c556ca427b425d7c5b43
NunMySelf <31649169+NunMySelf@users.noreply.github.com>
2018-01-07 15:59:17 +0700

    Create link.java

---
+++ link.java
@@ -0,0 +1,20 @@
+
+
+/**@Sakdinan khamnang 5907101013
+ */
+
+
+public class Link {
+	 public long data;
+	  public Link next;
+
+	  public Link(long val) {
+	    data = val;
+	    next = null;
+	  }
+
+	  public void displayLink() {
+	    System.out.print(data + " ");
+	  }
+
+	}  
